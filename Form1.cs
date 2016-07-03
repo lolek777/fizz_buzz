bff67cfcc1910834259056e4ab59fa99d41e8eed
lolek777 <lolek_7@interia.pl>
2016-07-03 22:12:37 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace fizz_buzz_rekord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int licz=1;
+
+        public void btn1_Click(object sender, EventArgs e)
+        {
+            fizz_buzz gra = new fizz_buzz();
+            liczba.Text = licz.ToString();
+            fizz_buzz.Text = gra.fizzbuzz(licz);
+            licz++;
+        }
+    }
+}
---
+++ fizz_buzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fizz_buzz_rekord
+{
+    class fizz_buzz
+    {
+        public string tekst;
+        
+        public string fizzbuzz(int liczba)
+        {
+            if (liczba % 3 == 0 && liczba % 5 == 0)
+                tekst = "FIZZ BUZZ";
+            else if (liczba % 3 == 0)
+                tekst = "Fizz";
+            else if (liczba % 5 == 0)
+                tekst = "Buzz";            
+            else
+                tekst = liczba.ToString();
+
+            return tekst;
+        }
+    }
+}
